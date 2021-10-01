ef1520efb67a391981ce5984c6a32ae3344c57df
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-10-01 15:30:21 -0500

    bare bones and pseudo code

---
+++ client.js
@@ -0,0 +1,36 @@
+console.log('JS');
+
+$(readyNow);
+let employees = [];
+let totalSalary = 0;
+
+//click listeners
+$('#submit').on('click', addEmployee)
+
+function readyNow() {
+    console.log('JQ');
+}
+
+//a function to add an employee to the employees array 
+function addEmployee(){
+    //get inputs from the employee
+    //create an employee object
+    //push the employee object to the employees array
+
+}
+
+//a function to display all employees on the DOM
+function displayEmployees() {
+    //clear the DOM
+    //loop through employees array
+    //append each employee to the table
+
+}
+
+//a function to loop through the employees and calculate total of monthly salaries
+//updates total salary costs and appends to the DOM
+
+function calculateMonthly() {
+    //loop through employees
+    //add annualSalary to totalSalary
+}
